05e9164a95ef238fb786916d5820883df5f20011
Martin Kukla <martin.kukla@cantab.net>
2024-11-19 12:36:14 +0000

    UNTESTED: move modelling code to use pytorch. Very basic blocks and functional

--- model_triton.py
+++ model_triton.py
@@ -45,197 +45,91 @@ def count_num_params(params):
 
 ### MODEL
 
-# def log_softmax(x_logits): # compute log_softmax from logits over the last dimension
-#     x_logits = x_logits - jnp.max(x_logits, axis=-1, keepdims=True)
-#     return x_logits - logsumexp(x_logits, axis=-1, keepdims=True)
+# TODO XXX: PyTorch has recently enabled using some very fast implementation of matmul - check it out!
 
-# def embed(layer_params, x): # input: 1 x
-#     return layer_params[0][x] * jnp.sqrt(layer_params[0].shape[1]) # since layer_params[0] is vocab_size x emb_dim
+def log_softmax(x_logits): # compute log_softmax from logits over the last dimension
+    x_logits = x_logits - torch.max(x_logits, axis=-1, keepdims=True)
+    return x_logits - torch.logsumexp(x_logits, axis=-1, keepdims=True)
 
-# def relu(x):
-#     return jnp.maximum(0, x)
+def embed(layer_params, x): # input: 1 x
+    return layer_params[0][x] * math.sqrt(layer_params[0].shape[1]) # since layer_params[0] is vocab_size x emb_dim
 
-# def gelu(x):
-#     k = jnp.sqrt(2/jnp.pi)
-#     return 0.5 * x * (1 + jnp.tanh(k * (x + 0.044715 * jnp.power(x,3))))
+def relu(x):
+    return torch.maximum(0, x)
 
-# def linear_fwd(layer_params, x): # input: seq_len x emb_dim
-#     return jnp.matmul(x, jnp.transpose(layer_params[0])) + layer_params[1][None, :] # since layer_params[0] is output_dim x emb_dim, layer_params[1] is output_dim
+def gelu(x):
+    k = math.sqrt(2/math.pi)
+    return 0.5 * x * (1 + torch.tanh(k * (x + 0.044715 * torch.power(x,3))))
 
-# def proj_fwd(layer_params, x): # input: seq_len x emb_dim
-#     return jnp.matmul(x, jnp.transpose(layer_params)) # since layer_params is output_dim x emb_dim
+def linear_fwd(layer_params, x): # input: seq_len x emb_dim
+    return torch.matmul(x, torch.transpose(layer_params[0], 0, 1)) + layer_params[1][None, :] # since layer_params[0] is output_dim x emb_dim, layer_params[1] is output_dim
 
-# def scaled_dot_prod_attn(qkv, mask, key, train=True): # inputs: seq_len x emb_dim, mask: seq_len(q) x seq_len(k)
-#     q, k, v = qkv
-#     attn = jnp.matmul(q, jnp.transpose(k)) # seq_len(q) x seq_len(k)
-#     attn = attn / jnp.sqrt(q.shape[-1]) # scale by sqrt(d_k)
-#     #attn = jnp.where(mask, attn, jnp.full_like(attn, -jnp.inf))
-#     attn = jnp.where(mask, attn, jnp.full_like(attn, -1e9)) # Note, instead of usign -jnp.inf, which results in NaNs (NIT: probably better to use jax.numpy.finfo)
-#     softmaxed_attn = jnp.exp(log_softmax(attn))
-#     softmaxed_attn = dropout(softmaxed_attn, key, train)
-#     return jnp.matmul(softmaxed_attn, v) # output: seq_len x emb_dim
+def proj_fwd(layer_params, x): # input: seq_len x emb_dim
+    return torch.matmul(x, torch.transpose(layer_params, 0, 1)) # since layer_params is output_dim x emb_dim
 
-# def tlayer_attn_head_fwd(layer_params, qkv, mask, key, train): # input: seq_len x emb_dim
-#     proj_qkv = tuple([proj_fwd(p, x) for p, x in zip(layer_params, qkv)]) #TODO: vmap? For cross attn, qkv are not of the same shape..
-#     return scaled_dot_prod_attn(proj_qkv, mask, key, train)
+def scaled_dot_prod_attn(qkv, mask, train=True): # inputs: seq_len x emb_dim, mask: seq_len(q) x seq_len(k)
+    q, k, v = qkv
+    attn = torch.matmul(q, torch.transpose(k, 0, 1)) # seq_len(q) x seq_len(k)
+    attn = attn / math.sqrt(q.shape[-1]) # scale by sqrt(d_k)
+    #attn = jnp.where(mask, attn, jnp.full_like(attn, -jnp.inf))
+    attn = torch.where(mask, attn, torch.full_like(attn, -1e9)) # Note, instead of usign -jnp.inf, which results in NaNs (NIT: probably better to use jax.numpy.finfo)
+    softmaxed_attn = torch.exp(log_softmax(attn))
+    softmaxed_attn = dropout(softmaxed_attn, train)
+    return torch.matmul(softmaxed_attn, v) # output: seq_len x emb_dim
 
-# tlayer_attn_heads_fwd = vmap(tlayer_attn_head_fwd, in_axes=(0, None, None, 0, None))
+def tlayer_attn_head_fwd(layer_params, qkv, mask, train): # input: seq_len x emb_dim
+    proj_qkv = tuple([proj_fwd(p, x) for p, x in zip(layer_params, qkv)]) #TODO: vmap? For cross attn, qkv are not of the same shape..
+    return scaled_dot_prod_attn(proj_qkv, mask, train)
 
-# def tlayer_attn_fwd(layer_params, qkv, mask, key, train): # input: seq_len x emb_dim
-#     num_heads = layer_params[0].shape[0]
-#     keys = random.split(key, num_heads)
-#     heads_attns = tlayer_attn_heads_fwd(layer_params[0], qkv, mask, keys, train)
-#     attn = jnp.concatenate(heads_attns, axis=-1)
-#     return proj_fwd(layer_params[-1], attn)
+tlayer_attn_heads_fwd = torch.vmap(tlayer_attn_head_fwd, in_axes=(0, None, None, None))
 
-# def tlayer_ffn_fwd(layer_params, x, activation_fn): # input: seq_len x emb_dim
-#     x = linear_fwd((layer_params[0], layer_params[1]), x)
-#     x = activation_fn(x)
-#     x = linear_fwd((layer_params[2], layer_params[3]), x)
-#     return x
+def tlayer_attn_fwd(layer_params, qkv, mask, key, train): # input: seq_len x emb_dim
+    num_heads = layer_params[0].shape[0]
+    heads_attns = tlayer_attn_heads_fwd(layer_params[0], qkv, mask, train)
+    attn = torch.concatenate(heads_attns, axis=-1)
+    return proj_fwd(layer_params[-1], attn)
 
-# def dropout(x, key, train=True):
-#     if not train: # As we jit the whole loss/inference, the train param is known at tracing time.
-#         return x * (1-DROPOUT_RATE)
+def tlayer_ffn_fwd(layer_params, x, activation_fn): # input: seq_len x emb_dim
+    x = linear_fwd((layer_params[0], layer_params[1]), x)
+    x = activation_fn(x)
+    x = linear_fwd((layer_params[2], layer_params[3]), x)
+    return x
+
+def dropout(x, train=True):
+    if not train: # As we jit the whole loss/inference, the train param is known at tracing time.
+        return x * (1-DROPOUT_RATE)
     
-#     return x * random.bernoulli(key, 1-DROPOUT_RATE, x.shape)
+    return x * torch.bernoulli(torch.full_like(x, 1-DROPOUT_RATE))
 
-# def layernorm_fwd(layer_params, x):
-#     x_mean = jnp.mean(x, axis=-1, keepdims=True)
-#     x_std = jnp.std(x, axis=-1, keepdims=True) # TODO XXX: Compute variance, add epsilon and take a sqrt instead (in order to avoid division by zero)
-#     normalized_x = (x - x_mean) / x_std
-#     return jnp.multiply(x, layer_params[0][None, :]) + layer_params[1][None, :] # since both layer_params are output_dim x
+def layernorm_fwd(layer_params, x):
+    x_mean = torch.mean(x, axis=-1, keepdims=True)
+    x_std = torch.std(x, axis=-1, keepdims=True) # TODO XXX: Compute variance, add epsilon and take a sqrt instead (in order to avoid division by zero)
+    normalized_x = (x - x_mean) / x_std
+    return torch.multiply(x, layer_params[0][None, :]) + layer_params[1][None, :] # since both layer_params are output_dim x
 
-# def tlayer_fwd_aiayn(layer_params, y, mask, key, train=True): # input: seq_len x emb_dim
-#     keys = random.split(key, 2)
+def tlayer_fwd_gpt2(layer_params, y, mask, train=True): # input: seq_len x emb_dim
 
-#     # TODO: Note that, unlike GPT2, AIAYN doesn't apply dropouts to attention weights.
-#     # To achieve that, we set tlayer_attn_fwd's dropout layer in evaluation mode (thus passing 0 rnd key + train=false)
-#     # This is somehow ugly. In future, when we have many attn mechanims, we should pass attn function instead..
-#     y = y + dropout(tlayer_attn_fwd(layer_params[:-8], (y, y, y), mask, random.PRNGKey(0), False), keys[0], train)
-#     y = layernorm_fwd(layer_params[-8:-6], y)
+    y_diff = layernorm_fwd(layer_params[:2], y)
+    y = y + dropout(tlayer_attn_fwd(layer_params[2:-6], (y_diff, y_diff, y_diff), mask, train), train)
 
-#     y = y + dropout(tlayer_ffn_fwd(layer_params[-6:-2], y, relu), keys[1], train)
-#     y = layernorm_fwd(layer_params[-2:], y)
-#     return y
+    y_diff = layernorm_fwd(layer_params[-6:-4], y)
+    y = y + dropout(tlayer_ffn_fwd(layer_params[-4:], y_diff, gelu), train)
+    return y
 
-# def tlayer_fwd_gpt2(layer_params, y, mask, key, train=True): # input: seq_len x emb_dim
-#     keys = random.split(key, 3)
-
-#     y_diff = layernorm_fwd(layer_params[:2], y)
-#     y = y + dropout(tlayer_attn_fwd(layer_params[2:-6], (y_diff, y_diff, y_diff), mask, keys[0], train), keys[1], train)
-
-#     y_diff = layernorm_fwd(layer_params[-6:-4], y)
-#     y = y + dropout(tlayer_ffn_fwd(layer_params[-4:], y_diff, gelu), keys[2], train)
-#     return y
-
-# def tlayer_with_cross_attn_fwd(layer_params, y, mask, x, yx_mask, key, train=True): # input: seq_len x emb_dim
-#     cross_attn_section = int((len(layer_params) - 6)/2)
-#     keys = random.split(key, 3)
-#     y = y + dropout(tlayer_attn_fwd(layer_params[:cross_attn_section-2], (y, y, y), mask), keys[0], train)
-#     y = layernorm_fwd(layer_params[cross_attn_section-2:cross_attn_section], y)
-
-#     #cross attn
-#     y = y + dropout(tlayer_attn_fwd(layer_params[cross_attn_section:-8], (y, x, x), yx_mask), keys[1], train)
-#     y = layernorm_fwd(layer_params[-8:-6], y)
+def tlayers_fwd_gpt2(params, y, mask, indices, train=True): # input: seq_len x
+    y = embed(params[0], y)
+    y = dropout(y + params[1][0], train)
     
-#     y = y + dropout(tlayer_ffn_fwd(layer_params[-6:-2], y, relu), keys[2], train)
-#     y = layernorm_fwd(layer_params[-2:], y)
-#     return y
+    for layer_params in params[2:-1]:
+        y = tlayer_fwd_gpt2(layer_params, y, mask, train)
+    y = layernorm_fwd(params[-1], y)
 
-# def pos_encodings(x, indices): # input: seq_len x emb_dim
-#     seq_len, emb_dim = x.shape
+    return y
 
-#     #indices = jnp.arange(seq_len)[:, None] 
-#     indices = indices[:, None] 
-#     div_term = jnp.fromfunction(lambda i: 1 / pow(10000, 2 * i/emb_dim), (int(emb_dim/2),), dtype=float)[None, :]
-#     pos_array = jnp.dot(indices, div_term)
-#     return jnp.stack((jnp.sin(pos_array), jnp.cos(pos_array)), axis=2).reshape(seq_len, emb_dim)
-
-# # TODO: unify tlayer(s) with tlayer(s)_with_cross_attention, but that jit works correctly
-# def tlayers_fwd_aiayn(params, y, mask, indices, key, train=True): # input: seq_len x
-#     key, dropout_key = random.split(key, 2)
-#     y = embed(params[0], y)
-#     y = dropout(y + pos_encodings(y, indices), dropout_key, train)
+def forward_gpt2(params, y, y_mask, y_indices, train): # input: seq_len x
+    y = tlayers_fwd_gpt2(params, y, y_mask, y_indices, train=train)
     
-#     for layer_params in params[1:]:
-#         key, tlayer_key = random.split(key, 2)
-#         y = tlayer_fwd_aiayn(layer_params, y, mask, tlayer_key, train)
+    y = linear_fwd(params[0], y) 
+    return y
 
-#     return y
-
-# def tlayers_fwd_gpt2(params, y, mask, indices, key, train=True): # input: seq_len x
-#     key, dropout_key = random.split(key, 2)
-#     y = embed(params[0], y)
-#     y = dropout(y + params[1][0], dropout_key, train)
-    
-#     for layer_params in params[2:-1]:
-#         key, tlayer_key = random.split(key, 2)
-#         y = tlayer_fwd_gpt2(layer_params, y, mask, tlayer_key, train)
-#     y = layernorm_fwd(params[-1], y)
-
-#     return y
-
-# def tlayers_fwd_aiayn_scanned(params, y, mask, key, train=True): # input: seq_len x
-#     key, dropout_key = random.split(key, 2)
-#     y = embed(params[0], y)
-#     y = dropout(y + pos_encodings(y), dropout_key, train)
-    
-#     def tlayer_fwd_scan_step(y_and_key, tlayer_params):
-#         y, key = y_and_key
-#         key, tlayer_key = random.split(key, 2)
-#         y = tlayer_fwd(tlayer_params, y, mask, tlayer_key, train)
-#         return (y, key), None
-
-#     y_and_key, _ = lax.scan(tlayer_fwd_scan_step, (y, key), params[1]) 
-    
-#     return y_and_key[0]
-
-# def tlayers_with_cross_attn_fwd(params, y, mask, x, yx_mask, y_indices, key, train=True): # input: seq_len x
-#     keys = random.split(key, len(params) )
-#     y = embed(params[0], y)
-#     y = dropout(y + pos_encodings(y, y_indices), keys[0], train)
-    
-#     for layer_params, layer_key in zip(params[1:], keys[1:]):
-#         y = tlayer_with_cross_attn_fwd(layer_params, y, mask, x, yx_mask, layer_key, train)
-#     return y
-
-# def tlayers_with_cross_attn_fwd_scanned(params, y, mask, x, yx_mask, key, train=True): # input: seq_len x
-#     key, dropout_key = random.split(key, 2)
-#     y = embed(params[0], y)
-#     y = dropout(y + pos_encodings(y), dropout_key, train)
-
-#     def tlayer_fwd_scan_step(y_and_key, tlayer_params):
-#         y, key = y_and_key
-#         key, tlayer_key = random.split(key, 2)
-#         y = tlayer_with_cross_attn_fwd(tlayer_params, y, mask, x, yx_mask, tlayer_key, train)
-#         return (y, key), None
-
-#     y_and_key, _ = lax.scan(tlayer_fwd_scan_step, (y, key), params[1]) 
-    
-#     return y_and_key[0]
-
-# def forward_aiayn(params, x, y, x_mask, y_mask, yx_mask, x_indices, y_indices, key, train): # input: seq_len x
-#     layers = int((len(params) -1) /2)
-#     keys = random.split(key, 2)
-    
-#     x = tlayers_fwd_aiayn(params[:layers+1], x, x_mask, x_indices, keys[0], train=train)
-#     #x = tlayers_fwd_aiayn_scanned(params[:2], x, x_mask, keys[0], train=train)
-    
-#     y = tlayers_with_cross_attn_fwd([params[0]] + params[layers+1:], y, y_mask, x, yx_mask, y_indices, keys[1], train=train)
-#     #y = tlayers_with_cross_attn_fwd_scanned([params[0], params[2]], y, y_mask, x, yx_mask, keys[1], train=train)
-#     y = linear_fwd(params[0], y) 
-#     return y
-
-# #batched_forward = jit(vmap(forward_aiyan, in_axes=(None, 0, 0, None, None)), static_argnames=['train'])
-# batched_forward_aiayn = vmap(forward_aiayn, in_axes=(None, 0, 0, 0, 0, 0, 0, 0, None, None))
-
-# def forward_gpt2(params, y, y_mask, y_indices, key, train): # input: seq_len x
-#     keys = random.split(key, 2)
-    
-#     y = tlayers_fwd_gpt2(params, y, y_mask, y_indices, keys[0], train=train)
-    
-#     y = linear_fwd(params[0], y) 
-#     return y
-
-# batched_forward_gpt2 = vmap(forward_gpt2, in_axes=(None, 0, 0, 0, None, None))
+batched_forward_gpt2 = torch.vmap(forward_gpt2, in_axes=(None, 0, 0, 0, None))
