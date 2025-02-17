a2d29ee766d1085e6715d702430865ab5f6dfc94
Martin Kukla <martin.kukla@cantab.net>
2025-02-17 12:04:15 +0000

    cleaning slicing

--- triton_extensions.py
+++ triton_extensions.py
@@ -1,14 +1,15 @@
 # Extensions for Triton Language
 
+import triton
 import triton.language as core
 
-# Ops for slicing (take/put) local tensor (inspired by https://github.com/triton-lang/triton/pull/2715)
+# Ops for slicing (take/put) local tensor (extension to https://github.com/triton-lang/triton/pull/2715)
 @triton.jit
 def _indicator_(n_dims: core.constexpr, idx: core.constexpr, pos: core.constexpr, pos_dim: core.constexpr):
     core.static_assert(idx < n_dims)
-    core.static_assert((pos>0) and (pos <= pos_dim))
-    y = core.arange(1, pos_dim+1)
-    y = tl.where(y==pos, 1, 0)
+    core.static_assert(pos < pos_dim)
+    y = core.arange(0, pos_dim)
+    y = core.where(y==pos, 1, 0)
     
     for n in core.static_range(0, n_dims):
         if n != n_dims - 1 - idx:
@@ -17,15 +18,16 @@ def _indicator_(n_dims: core.constexpr, idx: core.constexpr, pos: core.constexpr
 
 @triton.jit
 def _take_slice_(x, n_dims: core.constexpr, idx: core.constexpr, pos: core.constexpr, pos_dim:core.constexpr, keep_dim: core.constexpr = True):
-    ind = _indicator_(n_dims, idx, pos+1, pos_dim)
+    ind = _indicator_(n_dims, idx, pos, pos_dim)
     y = core.sum(x * ind, n_dims - 1 - idx)
     if keep_dim:
         y = core.expand_dims(y, n_dims - 1 - idx)
 
     return y
 
+# TODO T: note this op can use 1x tl.where in total instead of 2..
 @triton.jit
 def _put_slice_(x, n_dims: core.constexpr, idx: core.constexpr, pos: core.constexpr, pos_dim:core.constexpr, input_slice):
-    ind = _indicator_(n_dims, idx, pos+1, pos_dim)
-    y = tl.where(ind, input_slice, x)
+    ind = _indicator_(n_dims, idx, pos, pos_dim)
+    y = core.where(ind==1, input_slice, x)
     return y
