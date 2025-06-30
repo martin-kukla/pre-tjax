9d1bc4b1884f3469593b6c6ae2fb35eaed72fa3b
Martin Kukla <martin.kukla@cantab.net>
2025-06-30 10:19:48 +0000

    init test_t_tlayer_ffn_fwd.py

---
+++ tests/test_t_tlayer_ffn_fwd.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append('../')
+
+import torch
+from model_triton import t_tlayer_ffn_fwd, t_gelu_fwd, t_tlayer_ffn_fwd_t
+
+BS, N, D = 8, 512, 768
+FFN = 4 * D
+aa = torch.randn((BS, N, D), device="cuda")
+p0 = torch.randn((FFN, D), device="cuda")
+p1 = torch.randn((FFN,), device="cuda")
+p2 = torch.randn((D, FFN), device="cuda")
+p3 = torch.randn((D,), device="cuda")
+params = (p0, p1, p2, p3)
+
+res1 = t_tlayer_ffn_fwd(params, aa, t_gelu_fwd)
+print(res1[-2:, -4:, -8:])
+
+
+res2 = t_tlayer_ffn_fwd_t(params, aa, t_gelu_fwd)
+print(res2[-2:, -4:, -8:])
+
+assert torch.allclose(res1, res2, atol=1e-2, rtol=5e-4), (res1.shape, res2.shape, res1[-2:, -4:, -8:], res2[-2:, -4:, -8:])
+#assert torch.allclose(res1, res2, atol=1.7e-2, rtol=0), (res1.shape, res2.shape, res1[-4:, -4:], res2[-4:, -4:])
