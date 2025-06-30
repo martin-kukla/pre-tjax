a575aefc9d17e4734bf63b0986caf03727c4f510
Martin Kukla <martin.kukla@cantab.net>
2025-06-30 10:59:37 +0000

    test_t_dropout_bkwd.py against torch_func

--- tests/test_t_dropout_bkwd.py
+++ tests/test_t_dropout_bkwd.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.append('../')
 import torch
-from model_triton import t_dropout_bkwd2, t_dropout_bkwd2_t
+from model_torch_func import dropout
+from model_triton import t_dropout_bkwd2_t
 
 dloss_dx = torch.randn((8, 512, 768), device="cuda")
 #Two shapes are being used: [8, 12, 512, 512], and 4096, 35374
@@ -9,10 +10,13 @@ aa = torch.randn((8, 512, 768), device="cuda")
 #aa = torch.randn((4096, 35374), device="cuda")
 #aa = aa.view(-1)
 
-res1= t_dropout_bkwd2(dloss_dx, aa, train=True, p_gen_aux=42) 
-#print(res1)
+from functools import partial
+dropout_train = partial(dropout, train=True)
+(_, vjpfunc) = torch.func.vjp(dropout_train, aa)
+res1 = vjpfunc(dloss_dx)[0]
+# print(res1[-2:, -4:, -8:])
 
 res2 = t_dropout_bkwd2_t(dloss_dx, aa, train=True, p_gen_aux=42)
-#print(res2)
+# print(res2[-2:, -4:, -8:])
 
-assert torch.allclose(res1, res2), (res1[0], res2[0])
+assert torch.allclose(res1, res2), (res1[-2:, -4:, -8:], res2[-2:, -4:, -8:])
