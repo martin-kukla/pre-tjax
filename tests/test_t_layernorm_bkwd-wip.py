253673bcbc7edf378484e59bdbba468012f37089
Martin Kukla <martin.kukla@cantab.net>
2025-07-01 09:50:14 +0000

    iter test_t_layernorm_bkwd-wip.py

--- tests/test_t_layernorm_bkwd-wip.py
+++ tests/test_t_layernorm_bkwd-wip.py
@@ -1,7 +1,12 @@
+import os
+os.environ["TRITON_INTERPRET"] = "1"
+
+
 import sys
 sys.path.append('../')
+
 import torch
-from model_torch_func import layernorm_fwd
+#from model_torch_func import layernorm_fwd
 #from model_triton import t_layernorm_bkwd2_p_t, t_layernorm_bkwd2_x_t
 
 BS, N, D = 1, 1, 4 #8, 512, 768
@@ -15,14 +20,22 @@ print(f'dloss_dx', dloss_dx)
 print(f'aa', aa)
 
 
+print(f'\n### TORCH_FUNC')
 # dx
+def layernorm_fwd(layer_params, x):
+    x_mean = torch.mean(x, axis=-1, keepdims=True)
+    print("x_mean", x_mean)
+    x_std = torch.std(x, axis=-1, keepdims=True) # TODO XXX: Compute variance, add epsilon and take a sqrt instead (in order to avoid division by zero)
+    print("x_std", x_std)
+    normalized_x = (x - x_mean) / x_std
+    print("normalized_x", normalized_x)
+    return torch.multiply(normalized_x, layer_params[0][None, :]) + layer_params[1][None, :] # since both layer_params are output_dim x
+
 (_, vjpfunc) = torch.func.vjp(layernorm_fwd, layer_params, aa)
 res1 = vjpfunc(dloss_dx)[1]
-print(res1.shape, res1[-2:, -4:, -8:])
+print("RESULT:", res1.shape, res1[-2:, -4:, -8:])
 
-# import os
-# os.environ["TRITON_INTERPRET"] = "1"
-
+print(f'\n### TRITON')
 import triton
 import triton.language as tl
 
@@ -39,7 +52,7 @@ def t_layernorm_bkwd2_x_k(dloss_dx_ptr,
                     n_rows,
                     n_cols,
                     BLOCK_SIZE: tl.constexpr,
-                    num_stages: tl.constexpr,
+                    #num_stages: tl.constexpr,
                     ):
     row_start = tl.program_id(0)
     row_step = tl.num_programs(0)
@@ -50,7 +63,7 @@ def t_layernorm_bkwd2_x_k(dloss_dx_ptr,
     mask = offsets < n_cols
     param1 = tl.load(param1_ptr + offsets, mask=mask, other=0.0)  
         
-    for row_idx in tl.range(row_start, n_rows, row_step, num_stages):
+    for row_idx in tl.range(row_start, n_rows, row_step): #, num_stages):
         dloss_dx_row_start_ptr = dloss_dx_ptr + row_idx * dloss_dx_stride
         dloss_dx = tl.load(dloss_dx_row_start_ptr + offsets, mask=mask, other=0.0)
         dloss_dx = dloss_dx * param1
@@ -65,9 +78,12 @@ def t_layernorm_bkwd2_x_k(dloss_dx_ptr,
         x_minus_mu2_sum = tl.sum(x_minus_mu2, axis=0)
         x_sigma2 = x_minus_mu2_sum / (n_cols-1)
         x_sigma = tl.sqrt_rn(x_sigma2)
+        print('x_mu', x_mu)
+        print('x_sigma', x_sigma)        
         
         # normalize x
         x_norm = x_minus_mu/x_sigma    
+        print('x_norm', x_norm)        
         
         # bkwd quantities
         dloss_dx_sum = tl.sum(dloss_dx, axis=0)
@@ -95,12 +111,13 @@ def t_layernorm_bkwd2_x_t(dloss_dx:torch.Tensor, layer_params: torch.Tensor, x: 
     num_programs = min(n_rows, 480) 
     t_layernorm_bkwd2_x_k[(num_programs,)](dloss_dx_2d, layer_params[0], x_2d, output, 
                                        dloss_dx_2d.stride(0), x_2d.stride(0), output.stride(0), n_rows, n_cols, 
-                                       BLOCK_SIZE=BLOCK_SIZE, num_warps=num_warps, num_stages=num_stages)
+                                       BLOCK_SIZE=BLOCK_SIZE, num_warps=num_warps) #, num_stages=num_stages)
     return output.reshape(dloss_dx.shape)
 
 res2 = t_layernorm_bkwd2_x_t(dloss_dx, layer_params, aa)
-print(res2.shape, res2[-2:, -4:, -8:])
+print("RESULT:", res2.shape, res2[-2:, -4:, -8:])
 
+print("\n\n")
 atol, rtol=1e-1, 1e-3
 def pre_allclose(input, other, name, atol, rtol):
     allclose = torch.abs(input - other) - atol - rtol * torch.abs(other)
